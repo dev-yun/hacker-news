9ae1faeabab483a9214b69b011524a2d21b35868
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-07-09 15:21:48 +0900

    [CREATE] hacker news api 실습
    ajax를 통해 hacker news api의 json date를 사용하는 실습

--- dist/index.5ecc50b1.js
+++ dist/index.5ecc50b1.js
@@ -532,6 +532,19 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"lGjiT":[function(require,module,exports) {
+// ajax는 다른 서버의 데이터를 가져와 저장할 수 있게 돕는 도구
+const ajax = new XMLHttpRequest();
+let NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+ajax.open("GET", NEWS_URL, false);
+ajax.send();
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement("ul");
+for(let i = 0; i < 10; i++){
+    const li = document.createElement("li");
+    li.innerHTML = newsFeed[i].title;
+    ul.appendChild(li);
+}
+document.getElementById("root").appendChild(ul);
 
 },{}]},["AzEb7","lGjiT"], "lGjiT", "parcelRequire94c2")
 
