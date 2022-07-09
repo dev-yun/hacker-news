0f21e08c72ba249ef82964f77a34bca4f98b812e
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-07-09 17:19:12 +0900

    [UPDATE] DOM API 제거, router 생성
    남은 DOM API로 생성한 html을 문자열 html로 대체
    router 함수를 만들어 화면 전환 기능 추가

--- dist/index.5ecc50b1.js
+++ dist/index.5ecc50b1.js
@@ -535,38 +535,49 @@ function hmrAcceptRun(bundle, id) {
 // ajax는 다른 서버의 데이터를 가져와 저장할 수 있게 돕는 도구
 const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
-const content = document.createElement("div");
 let NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 let CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-// ajax.send() 이후 response로 JSON값을 받아올 수 있음
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
-// 페이지 내의 hash(#)값(주소)이 변하면 실행되는 함수
-window.addEventListener("hashchange", function() {
-    //location : 브라우저가 기본으로 제공하는 객체 (주소와 관련된 다양한 정보 제공)
-    const id = location.hash.slice(1);
-    const newsContent = getData(CONTENT_URL.replace("@id", id));
-    const title = document.createElement("h1");
-    title.innerHTML = newsContent.title;
-    content.appendChild(title);
-});
-for(let i = 0; i < 10; i++){
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <li>
-        <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-    </li>
-  `;
-    ul.appendChild(div.firstElementChild);
-}
-container.appendChild(ul);
-container.appendChild(content);
 function getData(url) {
     // ajax.open(응답방식, 주소, 비동기 boolean값)
     ajax.open("GET", url, false);
     ajax.send();
+    // ajax.send() 이후 response로 JSON값을 받아올 수 있음
     return JSON.parse(ajax.response);
 }
+function newsDetail() {
+    //location : 브라우저가 기본으로 제공하는 객체 (주소와 관련된 다양한 정보 제공)
+    const id = location.hash.slice(1);
+    const newsContent = getData(CONTENT_URL.replace("@id", id));
+    container.innerHTML = `
+      <h1>${newsContent.title}</h1>
+  
+      <div>
+          <a href='#'>목록으로</a>
+      </div>
+    `;
+}
+function newsFeedFuc() {
+    const newsFeed = getData(NEWS_URL);
+    // 배열을 사용하여 li태그들을 다루는 방법
+    const newsList = [];
+    newsList.push("<ul>");
+    for(let i = 0; i < 10; i++)newsList.push(`
+        <li>
+            <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+        </li>
+    `);
+    newsList.push("</ul>");
+    container.innerHTML = newsList.join("");
+}
+function router() {
+    const routePath = location.hash;
+    // location.hash에 '#'만 들어있으면 빈 문자열을 반환
+    if (routePath === "") newsFeedFuc();
+    else newsDetail();
+}
+// 페이지 내의 hash(#)값(주소)이 변하면 실행되는 함수
+window.addEventListener("hashchange", router);
+router();
 
 },{}]},["AzEb7","lGjiT"], "lGjiT", "parcelRequire94c2")
 
