3946d4dfa2cdd3cd1c6ed9349fef2dc5c127a56e
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-07-10 17:34:32 +0900

    [UPDATE] Page 구현, route 수정
    Page구현 실습을 진행
    Page가 변함에 있어 route의 수정도 병행

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// ajax는 다른 서버의 데이터를 가져와 저장할 수 있게 돕는 도구
-
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 let NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 let CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   // ajax.open(응답방식, 주소, 비동기 boolean값)
@@ -16,15 +17,15 @@ function getData(url) {
 
 function newsDetail() {
   //location : 브라우저가 기본으로 제공하는 객체 (주소와 관련된 다양한 정보 제공)
-  const id = location.hash.slice(1);
+  const id = location.hash.slice(7);
 
   const newsContent = getData(CONTENT_URL.replace('@id', id));
 
   container.innerHTML = `
       <h1>${newsContent.title}</h1>
-  
+      <p>${newsContent.user}</p>
       <div>
-          <a href='#'>목록으로</a>
+          <a href='#/page/${store.currentPage}'>목록으로</a>
       </div>
     `;
 }
@@ -36,15 +37,27 @@ function newsFeedFuc() {
 
   newsList.push('<ul>');
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
         <li>
-            <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
         </li>
     `);
   }
 
   newsList.push('</ul>');
+  newsList.push(`
+  <div>
+    <a href='#/page/${
+      store.currentPage > 1 ? store.currentPage - 1 : 1
+    }'>이전 페이지</a>
+    <a href='#/page/${
+      store.currentPage * 10 < newsFeed.length
+        ? store.currentPage + 1
+        : store.currentPage
+    }'>다음 페이지</a>
+  </div>
+  `);
 
   container.innerHTML = newsList.join('');
 }
@@ -54,6 +67,9 @@ function router() {
 
   // location.hash에 '#'만 들어있으면 빈 문자열을 반환
   if (routePath === '') {
+    newsFeedFuc();
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.slice(7));
     newsFeedFuc();
   } else {
     newsDetail();
