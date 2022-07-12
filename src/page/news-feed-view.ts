6bc2898624e248f896c4510c9f5c47938ab7fcae
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-07-13 01:06:01 +0900

    refactor : promise 를 async await으로 전환
    promise로 작성된 코드를 async await 패턴으로 작성하고 async await의 특징 학습

--- src/page/news-feed-view.ts
+++ src/page/news-feed-view.ts
@@ -37,20 +37,13 @@ export default class NewsFeedView extends View {
     this.store = store;
   }
 
-  render = (page: string = '1'): void => {
+  render = async (page: string = '1'): Promise<void> => {
     this.store.currentPage = Number(page);
 
     if (!this.store.hasFeeds) {
-        this.api.getDataWithPromise((feeds: NewsFeed[]) => {
-            this.store.setFeeds(feeds);
-            this.renderView();
-        })
+        this.store.setFeeds(await this.api.getData());
     }  
 
-    this.renderView();
-  }
-
-  renderView = () => {
     for(let i = (this.store.currentPage - 1) * 10; i < this.store.currentPage * 10; i++) {
         const { id, title, comments_count, user, points, time_ago, read } = this.store.getFeed(i);
   
@@ -83,4 +76,5 @@ export default class NewsFeedView extends View {
     
       this.updateView();
   }
+
 }
